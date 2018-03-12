4a7d77f55da6eb10e1c1e95ae0d7aaf0b22041c4
Stridemann <stridemann@gmail.com>
2018-03-12 19:44:30 +0200

    Initial commit

---
+++ src/BestiaryBeastCraft/Core.cs
@@ -0,0 +1,381 @@
+﻿using SharpDX;
+using SharpDX.Direct3D9;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using PoeHUD.Models.Enums;
+using PoeHUD.Plugins;
+using PoeHUD.Models;
+using PoeHUD.Poe.Components;
+using PoeHUD.Poe.RemoteMemoryObjects;
+using PoeHUD.Poe.FilesInMemory;
+
+namespace BestiaryBeastCraft
+{
+    public class Core : BaseSettingsPlugin<Settings>
+    {
+        public Core() { PluginName = "BestiaryBeastCraft"; }
+
+        private Dictionary<string, CraftMonster> MonsterMetadata = new Dictionary<string, CraftMonster>();
+        private Dictionary<string, CraftMonster>  MonsterMods = new Dictionary<string, CraftMonster>();
+        private List<MonsterDisplayCfg> TrackingMonsters = new List<MonsterDisplayCfg>();
+        private Dictionary<string, BestiaryCapturableMonster> MonstersUniversal = new Dictionary<string, BestiaryCapturableMonster>();
+        private Dictionary<string, int> CaptureGenusAmount = new Dictionary<string, int>();
+
+        public override void Initialise()
+        {
+            LoadRecipes();
+            CalcAmount();
+            GameController.Area.OnAreaChange += Area_OnAreaChange;
+        }
+
+        private void LoadRecipes()
+        {
+            foreach (var recipe in GameController.Files.BestiaryRecipes.EntriesList)
+            {
+                foreach (var component in recipe.Components)
+                {
+                    if (component.Mod != null)
+                    {
+                        if (!MonsterMods.TryGetValue(component.Mod.Key, out CraftMonster recipeCfg))
+                        {
+                            recipeCfg = new CraftMonster();
+                            recipeCfg.Mod = component.Mod;
+                            MonsterMods.Add(component.Mod.Key, recipeCfg);
+                        }
+                        recipeCfg.Recipes.Add(recipe);
+                    }
+
+                    if (component.BestiaryCapturableMonster != null)
+                    {
+                        if (!MonsterMetadata.TryGetValue(component.BestiaryCapturableMonster.MonsterVariety.VarietyId, out CraftMonster recipeCfg))
+                        {
+                            recipeCfg = new CraftMonster();
+                            MonsterMetadata.Add(component.BestiaryCapturableMonster.MonsterVariety.VarietyId, recipeCfg);
+                        }
+                        recipeCfg.Recipes.Add(recipe);
+                    }
+                }
+            }
+
+            foreach (var defMob in GameController.Files.BestiaryCapturableMonsters.EntriesList)
+            {
+                MonstersUniversal.Add(defMob.MonsterVariety.VarietyId, defMob);
+            }
+        }
+
+
+
+        private void Area_OnAreaChange(PoeHUD.Controllers.AreaController obj)
+        {
+            TrackingMonsters.Clear();
+        }
+
+        private class CraftMonster
+        {
+            public ModsDat.ModRecord Mod;
+            public List<BestiaryRecipe> Recipes = new List<BestiaryRecipe>();
+        }
+
+        private void CalcAmount()
+        {
+            CaptureGenusAmount.Clear();
+
+            foreach (var captMonster in GameController.Files.BestiaryCapturableMonsters.EntriesList)
+            {
+                if (!CaptureGenusAmount.ContainsKey(captMonster.BestiaryGenus.Name))
+                    CaptureGenusAmount.Add(captMonster.BestiaryGenus.Name, 0);
+                CaptureGenusAmount[captMonster.BestiaryGenus.Name] += GameController.Game.IngameState.ServerData.GetBeastCapturedAmount(captMonster);
+            }
+        }
+
+        public override void EntityAdded(EntityWrapper entityWrapper)
+        {
+            if (!entityWrapper.IsHostile) return;
+            if (!entityWrapper.HasComponent<Monster>()) return;
+            if (!entityWrapper.IsAlive) return;
+            var rareComps = entityWrapper.GetComponent<ObjectMagicProperties>();
+            if (rareComps == null) return;
+            if (rareComps.Rarity != MonsterRarity.Rare && rareComps.Rarity != MonsterRarity.Unique) return;
+
+            var path = entityWrapper.Path;
+            var sybstrIndc = path.IndexOf("@");
+            if (sybstrIndc != -1)
+                path = path.Substring(0, sybstrIndc);
+
+            MonstersUniversal.TryGetValue(path, out BestiaryCapturableMonster translatedMonster);
+            if (translatedMonster == null) return;
+
+            CalcAmount();
+
+            var captGenusAmount = CaptureGenusAmount[translatedMonster.BestiaryGenus.Name];
+            var newDisplayCfg = new MonsterDisplayCfg()
+            {
+                CaptMonster = translatedMonster,
+                CapturedGenusAmount = captGenusAmount,
+                Entity = entityWrapper,
+                LifeComp = entityWrapper.GetComponent<Life>(),
+                Rarity = rareComps.Rarity,
+                DisplayName = translatedMonster.MonsterName
+            };
+            TrackingMonsters.Add(newDisplayCfg);
+
+            if (MonsterMetadata.TryGetValue(path, out CraftMonster monsterNameCfg))
+            {
+                newDisplayCfg.Recipes.AddRange(monsterNameCfg.Recipes);
+            }
+
+            var modsFound = new List<string>();
+            foreach (var mod in rareComps.Mods)
+            {
+                if (MonsterMods.TryGetValue(mod, out CraftMonster monsterModCfg))
+                {
+                    modsFound.Add(monsterModCfg.Mod.UserFriendlyName);
+                    newDisplayCfg.Recipes.AddRange(monsterModCfg.Recipes);
+                }
+            }
+
+            newDisplayCfg.ModsCount = modsFound.Count;
+            if(newDisplayCfg.ModsCount > 0)
+                newDisplayCfg.DisplayMods = $"Mods: {string.Join(", ", modsFound)}";
+            newDisplayCfg.CaptureThreshould = CalcCatchThreshould(newDisplayCfg);
+        }
+
+        private float CalcCatchThreshould(MonsterDisplayCfg mobCfg)
+        {
+            var lifeComp = mobCfg.LifeComp;
+            float fullHp = lifeComp.MaxES + lifeComp.MaxHP;
+            float dmgToProcess = Settings.DPS.Value * 1000 * Settings.CaptureTime.Value;//3 sec
+
+            if(Settings.UpperThreshold.Value)
+            {
+                if(mobCfg.IsRed)
+                    dmgToProcess += fullHp * 0.1f;//On 10% hp
+                else
+                    dmgToProcess += fullHp * 0.2f;//On 20% hp
+            }
+
+            var result = dmgToProcess / fullHp;
+
+            if (Settings.HasCullingStrike.Value && result < 0.1f)
+                result = 0.1f;
+
+            if (Settings.Has20PrcCullingStrike.Value && result < 0.2f)
+                result = 0.2f;
+
+            if (result < 0)
+                result = 0;
+
+            if (result > 1)
+                result = 1;
+
+            return result;
+        }
+
+        public override void EntityRemoved(EntityWrapper entityWrapper) => TrackingMonsters.RemoveAll(x => x.Entity == entityWrapper);
+
+        private class MonsterDisplayCfg
+        {
+            public EntityWrapper Entity;
+            public Life LifeComp;
+            public List<BestiaryRecipe> Recipes = new List<BestiaryRecipe>();
+            public BestiaryCapturableMonster CaptMonster;
+            public int ModsCount;
+            public float CaptureThreshould;
+            public string DisplayName;
+            public string DisplayMods;
+            public int CapturedGenusAmount;
+            public MonsterRarity Rarity;
+
+            public bool IsRed => ModsCount > 1;
+            public bool IsYellow => ModsCount == 1;
+        }
+
+        public override void Render()
+        {
+            if (!GameController.InGame) return;
+            if (GameController.Game.IngameState.IngameUi.AtlasPanel.IsVisible) return;
+            if (GameController.Game.IngameState.IngameUi.TreePanel.IsVisible) return;
+            if (GameController.Area.CurrentArea.IsHideout) return;
+            if (GameController.Area.CurrentArea.IsTown) return;
+            if (GameController.Game.IngameState.IngameUi.InventoryPanel.IsVisible) return;
+        
+            var drawPosY = Settings.PosY.Value - Settings.Height.Value;
+
+            foreach (var monster in TrackingMonsters)
+            {
+                var life = monster.LifeComp;
+                int totalHp = life.MaxES + life.MaxHP;
+                int curHp = life.CurES + life.CurHP;
+                float perc = (float)curHp / totalHp;
+
+
+                if (Settings.DrawIcon.Value)
+                {
+                    var iconDrawPos = GameController.Game.IngameState.Camera.WorldToScreen(monster.Entity.Pos, monster.Entity);
+
+                    string icon = "images/bestiary-white.png";
+                    if (monster.IsRed)
+                        icon = "images/bestiary-red.png";
+                    else if (monster.IsYellow)
+                        icon = "images/bestiary-yellow.png";
+                    else if (monster.Rarity == MonsterRarity.Unique)
+                        icon = "images/bestiary-uniq.png";
+
+                    Graphics.DrawPluginImage(Path.Combine(PluginDirectory, icon), new RectangleF(
+                        iconDrawPos.X - Settings.IconSize.Value / 2,
+                        iconDrawPos.Y - Settings.IconSize.Value / 2,
+                        Settings.IconSize.Value, Settings.IconSize.Value));
+                }
+
+                Graphics.DrawText(perc.ToString("p0"), Settings.TextHeight.Value,
+                  new Vector2(Settings.PosX.Value - 5, drawPosY + Settings.Height.Value / 2), FontDrawFlags.VerticalCenter | FontDrawFlags.Right);
+
+                var displayLabel = monster.DisplayName;
+
+                if(!string.IsNullOrEmpty(monster.DisplayMods))
+                    displayLabel += $", {monster.DisplayMods}";
+
+                if (Settings.ShowGenus)
+                    displayLabel += $", ({monster.CaptMonster.BestiaryGenus.Name})";
+
+                DrawProgressBar(Settings.PosX.Value, drawPosY, GetColorByRarity(monster.Rarity), perc, monster);
+
+                Graphics.DrawText(displayLabel, Settings.TextHeight.Value, 
+                    new Vector2(Settings.PosX.Value + 5, drawPosY + Settings.Height.Value / 2), GetTextColorByRarity(monster.Rarity),
+                    FontDrawFlags.VerticalCenter | FontDrawFlags.Left);
+
+
+                string additionalInfo = "";
+
+                if (Settings.ShowHP.Value)
+                    additionalInfo += $"{(monster.LifeComp.CurES + monster.LifeComp.CurHP).KiloFormat()} ";
+
+                if (Settings.ShowAmountCaptured)
+                    additionalInfo += $"({monster.CapturedGenusAmount}/{monster.CaptMonster.BestiaryGenus.MaxInStorage})";
+
+                if (!string.IsNullOrEmpty(additionalInfo))
+                {
+                    Graphics.DrawText(additionalInfo, Settings.TextHeight.Value,
+                    new Vector2(Settings.PosX.Value + Settings.Width.Value - 5, drawPosY + Settings.Height.Value / 2), GetTextColorByRarity(monster.Rarity),
+                    FontDrawFlags.VerticalCenter | FontDrawFlags.Right);
+                }
+
+                if (Settings.ShowRecipes)
+                {
+                    var recipesSorted = (from recipe in monster.Recipes
+                        group recipe by recipe.Description
+                        into groupedDemoClass
+                        select groupedDemoClass).ToDictionary(
+                        gdc => gdc.Key, 
+                        gdc => string.Join(", ", gdc.Select(x => 
+                        x.HintText.Replace("Create a ", string.Empty).Replace("Create ", string.Empty).Replace(" with", string.Empty)
+                        ).ToArray()));
+
+                    var recipeStr = string.Join(", ", recipesSorted.Select(x => $"{x.Key}: {x.Value}"));
+
+                    Graphics.DrawText(recipeStr, Settings.TextHeight.Value,
+                    new Vector2(Settings.PosX.Value + Settings.Width.Value + 5, drawPosY + Settings.Height.Value / 2), Color.White, 
+                    FontDrawFlags.VerticalCenter | FontDrawFlags.Left);
+                }
+
+                if(Settings.ShowCatchThreshold)
+                {
+                    var deltaPos = Settings.PosX.Value + Settings.Width.Value * monster.CaptureThreshould;
+                    var pos1 = new Vector2(deltaPos, drawPosY - 3);
+                    var pos2 = pos1;
+                    pos2.Y += Settings.Height.Value + 6;
+
+                    Graphics.DrawLine(pos1, pos2, 2, Color.Green);
+                }
+
+                drawPosY -= Settings.Height.Value;
+                drawPosY -= Settings.Spacing.Value;
+            }
+        }
+
+        private void DrawProgressBar(int drawPosX, int drawPosY, Color barColor, float perc, MonsterDisplayCfg monsterCfg)
+        {
+            var mainRect = new RectangleF(drawPosX, drawPosY, Settings.Width.Value, Settings.Height.Value);
+            Graphics.DrawBox(mainRect, Settings.BGColor.Value);
+            var frameRect = mainRect;
+            mainRect.Width *= perc;
+            Graphics.DrawBox(mainRect, barColor);
+            
+            int borderSize = 1;
+            var drawBorderColor = Color.White;
+
+            if (monsterCfg.IsRed)
+            {
+                if (monsterCfg.Rarity == MonsterRarity.Unique)
+                {
+                    drawBorderColor = Color.DarkOrange;
+                    borderSize = 6;
+                }
+                else
+                {
+                    drawBorderColor = Color.Red;
+                    borderSize = 4;
+                }
+            }
+            else if (monsterCfg.IsYellow)
+            {
+                drawBorderColor = Color.Red;
+                borderSize = 2;
+            }
+            else if (monsterCfg.Rarity == MonsterRarity.Unique)
+            {
+                drawBorderColor = Color.DarkOrange;
+                borderSize = 1;
+            }
+
+            Graphics.DrawFrame(frameRect, borderSize, drawBorderColor);
+        }
+
+        private Color GetColorByRarity(MonsterRarity rarity)
+        {
+            switch(rarity)
+            {
+                case MonsterRarity.Unique:
+                    return Settings.UniqueHpColor.Value;
+                case MonsterRarity.Rare:
+                    return Settings.RareHpColor.Value;
+                default:
+                    return Color.Gray;
+            }
+        }
+
+        private Color GetTextColorByRarity(MonsterRarity rarity)
+        {
+            switch (rarity)
+            {
+                case MonsterRarity.Unique:
+                    return Settings.UniqueTextColor.Value;
+                case MonsterRarity.Rare:
+                    return Settings.RareTextColor.Value;
+                default:
+                    return Color.Gray;
+            }
+        }
+    }
+
+    public static class HpExtensions
+    {
+        public static string KiloFormat(this int num)
+        {
+            if (num >= 100000000)
+                return (num / 1000000).ToString("#,0M");
+
+            if (num >= 10000000)
+                return (num / 1000000).ToString("0.#") + "M";
+
+            if (num >= 100000)
+                return (num / 1000).ToString("#,0K");
+
+            if (num >= 10000)
+                return (num / 1000).ToString("0.#") + "K";
+
+            return num.ToString("#,0");
+        }
+    }
+}
---
+++ src/BestiaryBeastCraft/Settings.cs
@@ -0,0 +1,107 @@
+﻿using System.Windows.Forms;
+using SharpDX;
+using PoeHUD.Plugins;
+using PoeHUD.Hud.Settings;
+
+namespace BestiaryBeastCraft
+{
+    public class Settings : SettingsBase
+    {
+        public Settings()
+        {
+            Enable = true;
+            
+            Width = new RangeNode<int>(700, 0, 1000);
+            Height = new RangeNode<int>(20, 0, 100);
+            Spacing = new RangeNode<int>(5, 0, 100);
+            TextHeight = new RangeNode<int>(15, 5, 50);
+            CaptureTime = new RangeNode<int>(3, 0, 10);
+
+            BGColor = Color.Black;
+            UniqueHpColor = Color.Brown;
+            RareHpColor = Color.Orange;
+            UniqueTextColor = Color.White;
+            RareTextColor = Color.Black;
+
+            DPS = new RangeNode<int>(150, 0, 2000);
+            ShowCatchThreshold = true;
+            HasCullingStrike = false;
+            UpperThreshold = false;
+            Has20PrcCullingStrike = false;
+        }
+
+        [Menu("Show Recipes", 0)]
+        public ToggleNode ShowRecipes { get; set; } = true;
+
+        [Menu("Short Description", 1, 0)]
+        public ToggleNode RecipesShortDescription { get; set; } = false;
+
+        [Menu("Show Genus")]
+        public ToggleNode ShowGenus { get; set; } = false;
+
+        [Menu("Show Amount Captured")]
+        public ToggleNode ShowAmountCaptured { get; set; } = true;
+
+        [Menu("Show HP")]
+        public ToggleNode ShowHP { get; set; } = true;
+
+        [Menu("Pos X")]
+        public RangeNode<int> PosX { get; set; } = new RangeNode<int>(500, 0, 2000);
+
+        [Menu("Pos Y")]
+        public RangeNode<int> PosY { get; set; } = new RangeNode<int>(1000, 0, 2000);
+
+        [Menu("Width")]
+        public RangeNode<int> Width { get; set; }
+
+        [Menu("Height")]
+        public RangeNode<int> Height { get; set; }
+
+        [Menu("TextHeight")]
+        public RangeNode<int> TextHeight { get; set; }
+
+        [Menu("Spacing")]
+        public RangeNode<int> Spacing { get; set; }
+
+        [Menu("BG Color")]
+        public ColorNode BGColor { get; set; }
+
+        [Menu("Unique HP Color")]
+        public ColorNode UniqueHpColor { get; set; }
+
+        [Menu("Rare HP Color")]
+        public ColorNode RareHpColor { get; set; }
+
+        [Menu("Unique Text Color")]
+        public ColorNode UniqueTextColor { get; set; }
+
+        [Menu("Rare Text Color")]
+        public ColorNode RareTextColor { get; set; }
+
+
+        [Menu("Show Catch Threshold", 100)]
+        public ToggleNode ShowCatchThreshold { get; set; }
+
+        [Menu("Upper (min) Threshold", "To kill (false) or to Capture (true) threshold. Set false if you have enough DPS to kill it fast", 101, 100)]
+        public ToggleNode UpperThreshold { get; set; }
+
+        [Menu("DPS (K)", 110, 100)]
+        public RangeNode<int> DPS { get; set; }
+
+        [Menu("Has Culling Strike", 120, 100)]
+        public ToggleNode HasCullingStrike { get; set; }
+
+        [Menu("Slayer (20% CS)", 121, 120)]
+        public ToggleNode Has20PrcCullingStrike { get; set; }
+
+        [Menu("Net Capture Time", "By default 3sec", 130, 100)]
+        public RangeNode<int> CaptureTime { get; set; }
+
+
+        [Menu("Draw Icon", 200)]
+        public ToggleNode DrawIcon { get; set; } = true;
+
+        [Menu("Height", 210, 200)]
+        public RangeNode<int> IconSize { get; set; } = new RangeNode<int>(70, 10, 200);
+    }
+}
